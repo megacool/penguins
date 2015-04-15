8a8b4ffd761582eab7101298fb8986ef845ba38d
Skye <gskye@aol.com>
2015-04-14 23:46:26 -0400

    Tweaked player angular impulse

--- PajamaPenguins_Shared/PPGameScene.m
+++ PajamaPenguins_Shared/PPGameScene.m
@@ -441,8 +441,8 @@ CGFloat const kParallaxMinSpeed = -20.0;
     [player.physicsBody setVelocity:CGVectorMake(0, 0)];
     [player.physicsBody setCollisionBitMask:0x0];
     [player.physicsBody setContactTestBitMask:0x0];
-    [player.physicsBody applyImpulse:CGVectorMake(1, 15)];
-    [player.physicsBody applyAngularImpulse:-.00025];
+    [player.physicsBody applyImpulse:CGVectorMake(1, 75)];
+    [player.physicsBody applyAngularImpulse:-.00325];
 }
 
 - (void)updatePlayer:(NSTimeInterval)dt {
